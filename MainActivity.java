cd2988953232b472b10c6a225c50bedd4e8b8608
moticon <soonerpat@gmail.com>
2017-01-06 15:29:38 -0600

    test

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.moticon.testcreation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // a comment to change the file!
+    }
+}
